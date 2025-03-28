34e42d2bf6960bfc845a47b0c55386a5d3f7305c
ImDestiny <89016609+ImDestiny@users.noreply.github.com>
2025-03-28 14:11:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ class Config:
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
     DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
