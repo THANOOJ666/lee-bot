7b4e68d8d67171e67b1ed41c9565e618548dac41
Thanuj <39941386+THANOOJ666@users.noreply.github.com>
2025-05-04 18:08:30 +0530

    Update generate_session.py

--- generate_session.py
+++ generate_session.py
@@ -3,6 +3,6 @@ from pyrogram import Client
 api_id = 27866551
 api_hash = "76057a79a74262e29d6de1e9f41aab0d"
 
-app = Client("slow1", api_id=api_id, api_hash=api_hash)
+app = Client("slow2", api_id=api_id, api_hash=api_hash)
 
 app.run()
