92b5386ba7feb5c62fd6fb8d9f19a62f02afeb6f
Thanuj <39941386+THANOOJ666@users.noreply.github.com>
2025-05-04 20:25:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ class Config:
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "slowf3") 
+    BOT_SESSION = environ.get("BOT_SESSION", "slow") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
     DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "slowf3")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "slow")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
